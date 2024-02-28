2c314da0a092aae340b6dbc4483aec161a2c2fdd
Kosisookeke <k.okeke@alustudent.com>
2024-02-28 14:16:15 +0200

    api

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
+    
     if response.status_code == 200:
         data = response.json().get('data')
         if data:
